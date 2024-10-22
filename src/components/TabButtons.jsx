47b9a85f312646338309a4a359b695541546f581
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-10-22 20:55:12 +0530

    Delete all files in branch-name

--- src/components/TabButtons.jsx
+++
@@ -1,6 +0,0 @@
-export default function TabButtons({onSelect,children,isSelected}) {
-
-    return(
-        <li><button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button></li>
-    )
-}
