a70828190bf28918fc98d557359790129e2f13a7
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-21 18:14:34 +0530

    code has been updated

--- 09-Working-with-useEffect/vite.config.js
+++ 09-Working-with-useEffect/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from other devices on the network
+    port: 5173, // Use the same port as your Vite server
+  }
 })
