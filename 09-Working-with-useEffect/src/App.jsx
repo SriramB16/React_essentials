efefe6ee1536fd214fe46eed5fbda424030c8fa3
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-22 12:06:29 +0530

    code has been updated

--- 09-Working-with-useEffect/src/App.jsx
+++ 09-Working-with-useEffect/src/App.jsx
@@ -7,6 +7,8 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
@@ -42,6 +44,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds]),
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -49,6 +59,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current),
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)),
+    );
   }
 
   return (
