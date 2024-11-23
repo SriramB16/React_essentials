f92d312e1173f9615152674e8084790da1e060f6
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-23 13:01:48 +0530

    quiz component and questions.js has been added

---
+++ 10-Building-a-Quiz-App/src/components/Quiz.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Quiz () {
+    return(
+        <div>
+            <div>
+                <h2></h2>
+                <ul>
+
+                </ul>
+            </div>
+        </div>
+    )
+}
