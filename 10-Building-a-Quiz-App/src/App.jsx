f065fe0cf66f3e1cd75b610eccb683c4c457df24
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-23 13:45:56 +0530

    quiz comp code

--- 10-Building-a-Quiz-App/src/App.jsx
+++ 10-Building-a-Quiz-App/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header.jsx";
 
+
 function App() {
     return (
         <>
--- 10-Building-a-Quiz-App/src/components/Quiz.jsx
+++ 10-Building-a-Quiz-App/src/components/Quiz.jsx
@@ -1,12 +1,30 @@
-
+import QUESTIONS from '../question.js';
+import { useState } from "react";
+import question from "../question.js";
 
 export default function Quiz () {
-    return(
-        <div>
-            <div>
-                <h2></h2>
-                <ul>
+    const [userAnswer, setUserAnswer] = useState([]);
+    
+    const activeQuestionIndex = userAnswer.length;
 
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswer((prevUserAnswer) => {
+        return [...prevUserAnswer, selectedAnswer]
+        })
+    }
+    
+    return(
+        <div id='quiz'>
+            <div id='question'>
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id='answers'>
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                        <li key={answer} className='answer'>
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
