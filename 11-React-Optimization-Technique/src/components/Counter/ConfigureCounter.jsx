1b570019171983e57855ecd0e58f6b08b9867a9e
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-26 22:49:16 +0530

    structured through different component

--- 11-React-Optimization-Technique/src/components/Counter/ConfigureCounter.jsx
+++ 11-React-Optimization-Technique/src/components/Counter/ConfigureCounter.jsx
@@ -1,6 +1,10 @@
 export default function ConfigureCounter() {
 
     return (
-
+        <section id="configure-counter">
+            <h2>Set Counter</h2>
+            <input type="number" onChange={handleChange} value={enteredNumber}/>
+            <button onClick={handleSetClick}>Set</button>
+        </section>
     )
 }
