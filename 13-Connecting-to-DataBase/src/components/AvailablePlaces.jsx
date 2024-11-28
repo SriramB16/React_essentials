b818d1709a293dcd626734c4b73518fb95b6e3ae
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-28 21:19:29 +0530

    Error modal and error message has been updated

--- 13-Connecting-to-DataBase/src/components/AvailablePlaces.jsx
+++ 13-Connecting-to-DataBase/src/components/AvailablePlaces.jsx
@@ -1,21 +1,41 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import ErrorPage from "./ErrorPage.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
-      setIsFetching(false)
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, Please try again later",
+        });
+      }
+
+      setIsFetching(false);
     }
 
     fetchPlaces();
-  }, [])
+  }, []);
+
+  if(error) {
+    return <ErrorPage title='An Error occured' message={error.message} />;
+  }
 
   // useEffect( () => {
   //   fetch("http://localhost:3000/places")
@@ -32,7 +52,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
       title="Available Places"
       places={availablePlaces}
       isLoading={isFetching}
-      loadingText={'Fetching Place Data...'}
+      loadingText={"Fetching Place Data..."}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
