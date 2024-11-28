309dea918a5e01fe9c3e3876ac622500b0d40512
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-11-28 13:28:48 +0530

    Error Boundary files has been updated

--- 12-Class-Based-Components/src/components/UserFinder.js
+++ 12-Class-Based-Components/src/components/UserFinder.js
@@ -1,33 +1,31 @@
-import { Fragment, Component } from "react";
+import { Fragment, useState, useEffect, Component } from 'react';
 
-import Users from "./Users";
-import classes from "./UserFinder.module.css";
-import UsersContext from "../store/users-context";
-
-// const DUMMY_USERS = [
-//   { id: "u1", name: "Max" },
-//   { id: "u2", name: "Manuel" },
-//   { id: "u3", name: "Julie" },
-// ];
+import Users from './Users';
+import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context';
+import ErrorBoundary from './ErrorBoundary';
 
 class UserFinder extends Component {
-  static contextType =  UsersContext ;
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
       filteredUsers: [],
-      searchTerm: "",
+      searchTerm: '',
     };
   }
+
   componentDidMount() {
-    this.setState({filteredUsers: this.context.users});
+    // Send http request...
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
         filteredUsers: this.context.users.filter((user) =>
-          user.name.includes(this.state.searchTerm),
+            user.name.includes(this.state.searchTerm)
         ),
       });
     }
@@ -39,40 +37,40 @@ class UserFinder extends Component {
 
   render() {
     return (
-      <Fragment>
+        <Fragment>
           <div className={classes.finder}>
-            <input
-              type="search"
-              onChange={this.searchChangeHandler.bind(this)} />
+            <input type='search' onChange={this.searchChangeHandler.bind(this)} />
           </div>
-          <Users users={this.state.filteredUsers} />
-      </Fragment>
+          <ErrorBoundary>
+            <Users users={this.state.filteredUsers} />
+          </ErrorBoundary>
+        </Fragment>
     );
   }
 }
 
 // const UserFinder = () => {
-//     const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
-//     const [searchTerm, setSearchTerm] = useState('');
-//
-//     useEffect(() => {
-//         setFilteredUsers(
-//             DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
-//         );
-//     }, [searchTerm]);
-//
-//     const searchChangeHandler = (event) => {
-//         setSearchTerm(event.target.value);
-//     };
-//
-//     return (
-//         <Fragment>
-//             <div className={classes.finder}>
-//                 <input type='search' onChange={searchChangeHandler} />
-//             </div>
-//             <Users users={filteredUsers} />
-//         </Fragment>
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
 //     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type='search' onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
 // };
 
 export default UserFinder;
