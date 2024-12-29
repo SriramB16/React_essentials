a68e044a3d6dd57480f795461e83e3aca0cbaa7e
Sriram <136231040+SriramB16@users.noreply.github.com>
2024-12-29 21:08:56 +0530

    ref way

--- 15-Forms-and-User-Inputs/src/components/Login.jsx
+++ 15-Forms-and-User-Inputs/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+ import { useRef, useState } from "react";
 
 export default function Login() {
   const [emailIsInvalid, setEmailIsInvalid] = useState(false);
