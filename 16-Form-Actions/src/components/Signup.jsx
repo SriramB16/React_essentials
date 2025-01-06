3b7648e278c595eddb2fa9fb0bc44fe2d659018c
Sriram <136231040+SriramB16@users.noreply.github.com>
2025-01-06 22:49:05 +0530

    useActionState hook is used

--- 16-Form-Actions/src/components/Signup.jsx
+++ 16-Form-Actions/src/components/Signup.jsx
@@ -1,8 +1,10 @@
 import { isEmail, isNotEmpty, hasMinLength, isEqualToOtherValue } from '../util/validation.js'
+import {useActionState} from "react";
+
 
 export default function Signup() {
 
-    function signUpAction(formData) {
+    function signUpAction(prevFormState, formData) {
         const email =  formData.get("email");
         const password =  formData.get("password");
         const confirmPassword =  formData.get("confirm-password");
@@ -42,11 +44,18 @@ export default function Signup() {
             errors.push("You must select at least one acquisition channel");
         }
 
+        if (errors.length > 0) {
+            return { errors };
+        }
+
+        return { errors: null };
     }
 
 
+    const [formState,formAction] = useActionState(signUpAction, { errors: null,});
+
   return (
-    <form action={signUpAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -130,6 +139,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+        {formState.errors && (
+            <ul className="error">
+                {formState.errors.map((error) => (
+                    <li key={error}>{error}</li>
+                ))}
+            </ul>
+        )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
